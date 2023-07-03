b60fbc0230d5c074f713036ab48c0bae0fe0c8ae
Rod Melo <fromflux@users.noreply.github.com>
2023-07-04 00:38:08 +0100

    Update VehicleList tests

--- src/components/VehicleList/__tests__/VehicleList.test.js
+++ src/components/VehicleList/__tests__/VehicleList.test.js
@@ -5,6 +5,38 @@ import useData from '../useData';
 
 jest.mock('../useData');
 
+const MOCK_VEHICLE = {
+  id: 'xe',
+  modelYear: 'k17',
+  apiUrl: '/api/vehicle_xe.json',
+  media: [
+    {
+      name: 'vehicle',
+      url: '/images/16x9/xe_k17.jpg'
+    },
+    {
+      name: 'vehicle',
+      url: '/images/1x1/xe_k17.jpg'
+    }
+  ],
+  description: 'The most advanced, efficient and refined sports saloon that Jaguar has ever produced',
+  price: '£30,000',
+  meta: {
+    passengers: 5,
+    drivetrain: [
+      'AWD',
+      'RWD'
+    ],
+    bodystyles: [
+      'saloon'
+    ],
+    emissions: {
+      template: 'CO2 Emissions $value g/km',
+      value: 99
+    }
+  }
+};
+
 describe('<VehicleList /> Tests', () => {
   it('Should show loading state if it not falsy', () => {
     useData.mockReturnValue([true, 'An error occurred', 'results']);
@@ -25,11 +57,20 @@ describe('<VehicleList /> Tests', () => {
   });
 
   it('Should show results if loading successfully finished', () => {
-    useData.mockReturnValue([false, false, 'results']);
+    useData.mockReturnValue([false, false, []]);
     const { queryByTestId } = render(<VehicleList />);
 
     expect(queryByTestId('loading')).toBeNull();
     expect(queryByTestId('error')).toBeNull();
     expect(queryByTestId('results')).not.toBeNull();
+  });
+
+  it('Should show vehicle card if loading successfully finished', () => {
+    useData.mockReturnValue([false, false, [MOCK_VEHICLE]]);
+    const { queryByTestId, queryByText } = render(<VehicleList />);
+
+    expect(queryByTestId('loading')).toBeNull();
+    expect(queryByTestId('error')).toBeNull();
+    expect(queryByText(MOCK_VEHICLE.description)).not.toBeNull();
   });
 });
